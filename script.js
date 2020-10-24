14ee614abf003d995793a9f24f591a9ef22b0dc2
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-24 23:25:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('For solution open js file');
+
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if(num % 2 === 0) {
+       console.log('even');
+   }
+   //AND THIS LINE ↑↑↑↑↑
+}
